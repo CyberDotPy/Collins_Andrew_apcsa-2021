620ddacdd20baa3db4a84b0ad6fae829dffc6838
CyberDotPy <56804602+CyberDotPy@users.noreply.github.com>
2021-02-01 12:14:39 -0800

    Finished Unit02
    Also added a personal dictionary

---
+++ Unit02/src/Cube.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Andrew Collins
+//Date - 2/1/21
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		return 6 * (side * side);
+	}
+}
---
+++ Unit02/src/Trap.java
@@ -0,0 +1,13 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Andrew Collins
+//Date - 2/1/21
+//Class - Period 3
+//Lab  - Unit 2 Trap
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return 0.5 * (base1 + base2) * height;
+	}
+}
